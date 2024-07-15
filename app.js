dfd158c93a557820079b2715dbba694e40b752d3
Kevin Rengifo <157091616+Kevin-Rengifo0124@users.noreply.github.com>
2024-07-15 16:51:06 -0500

    Add files via upload
    Cambios de funciones duplicadas.

--- app.js
+++ app.js
@@ -1,76 +1,93 @@
-let numeroSecreto = 0;
-let intentos = 0;
-let listaNumerosSorteados = [];
-let numeroMaximo = 10;
+const logoPersona = document.querySelector(".imagen_logo_persona");
+const texto_1 = document.getElementById("texto_1");
+const texto_2 = document.getElementById("texto_2");
+const resultadoTexto = document.querySelector(".resultado_texto");
+const resultadoContenedor = document.querySelectorAll(".ocultar");
+const copiarBoton = document.getElementById("boton_copiar");
+const contenedorTexto = document.getElementById("contenedor_texto");
 
+const matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"]
+];
 
-
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
-function verificarIntento() {
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    
-    if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
-        document.getElementById('reiniciar').removeAttribute('disabled');
-    } else {
-        //El usuario no acertó.
-        if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('p','El número secreto es menor');
-        } else {
-            asignarTextoElemento('p','El número secreto es mayor');
-        }
-        intentos++;
-        limpiarCaja();
+function validarEntrada(texto) {
+    if (texto.trim() === "") {
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+        return false;
     }
-    return;
+    return true;
 }
 
-function limpiarCaja() {
-    document.querySelector('#valorUsuario').value = '';
-}
+function encriptarBoton() {
+    const entrada = contenedorTexto.value;
 
-function generarNumeroSecreto() {
-    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
-
-    console.log(numeroGenerado);
-    console.log(listaNumerosSorteados);
-    //Si ya sorteamos todos los números
-    if (listaNumerosSorteados.length == numeroMaximo) {
-        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
-    } else {
-        //Si el numero generado está incluido en la lista 
-        if (listaNumerosSorteados.includes(numeroGenerado)) {
-            return generarNumeroSecreto();
-        } else {
-            listaNumerosSorteados.push(numeroGenerado);
-            return numeroGenerado;
-        }
+    if (!validarEntrada(entrada)) {
+        return;
     }
+
+    ocultar();
+    const salida = encriptar(entrada);
+    resultadoTexto.textContent = salida;
+
+    contenedorTexto.value = "";
+    copiarBoton.innerText = "Copiar";
 }
 
-function condicionesIniciales() {
-    asignarTextoElemento('h1','Juego del número secreto!');
-    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
-    numeroSecreto = generarNumeroSecreto();
-    intentos = 1;
-    console.log(numeroSecreto);
+function desencriptarBoton() {
+    const entradaTexto = contenedorTexto.value;
+
+    if (!validarEntrada(entradaTexto)) {
+        return;
+    }
+
+    ocultar();
+    const salida = desencriptar(entradaTexto);
+    resultadoTexto.textContent = salida;
+
+    contenedorTexto.value = "";
+    copiarBoton.innerText = "Copiar";
 }
 
-function reiniciarJuego() {
-    //limpiar caja
-    limpiarCaja();
-    //Indicar mensaje de intervalo de números 
-    //Generar el número aleatorio
-    //Inicializar el número intentos
-    condicionesIniciales();
-    //Deshabilitar el botón de nuevo juego
-    document.querySelector('#reiniciar').setAttribute('disabled','true');
-    
+function encriptar(texto) {
+    matrizCodigo.forEach(([elemento, encriptado]) => {
+        texto = texto.replaceAll(elemento, encriptado);
+    });
+    return texto;
 }
 
-condicionesIniciales();
+function desencriptar(texto) {
+    matrizCodigo.forEach(([elemento, encriptado]) => {
+        texto = texto.replaceAll(encriptado, elemento);
+    });
+    return texto;
+}
+
+function ocultar() {
+    logoPersona.classList.add("ocultar");
+    texto_1.classList.add("ocultar");
+    texto_2.classList.add("ocultar");
+    resultadoContenedor[0].classList.add("activate");
+    resultadoContenedor[1].classList.add("activate");
+    resultadoTexto.classList.add("resultado_contenedor");
+}
+
+copiarBoton.addEventListener('click', () => {
+    navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+        if (result.state === "granted" || result.state === "prompt") {
+            const contenido = resultadoTexto.textContent;
+            const entradaTemporal = document.createElement("textarea");
+
+            entradaTemporal.value = contenido;
+            document.body.appendChild(entradaTemporal);
+            entradaTemporal.select();
+            document.execCommand("copy");
+            document.body.removeChild(entradaTemporal);
+
+            copiarBoton.innerText = "¡Copiado!";
+        }
+    });
+});
